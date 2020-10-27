0ed00b6732ca5f2ea359fe6de8bb87e89e0e0be7
Sam.An <56215891+sammiee5311@users.noreply.github.com>
2020-10-27 23:21:19 +0900

    Update speech.py

--- speech.py
+++ speech.py
@@ -1,12 +1,14 @@
 from gtts import gTTS
 import os
 import os.path
-from music_player import Music_Player
-import music_player
-from youtube import Youtube_Player
-import time
-
+import numpy as np
 from playsound import playsound
+
+#####################################
+from music_player import Music_Player
+from youtube import Youtube_Player
+from guessing_game import q_learning
+#####################################
 
 
 # p = vlc.MediaPlayer("file://abc.mp3")
@@ -26,60 +28,112 @@ class speaker:
 
     def main(self, text):
         if text == 'youtube':
-            YT.flag = False
-            self.say("Please, write song\'s name")
+            YT.is_existed_playlist = False
+            self.say("Please, write music\'s name")
             self.delete()
-            song = input()
-            tracks = YT.search(str(song))
-            if not tracks:
-                self.say("Sorry, there is not %s on youtube" % str(song))
+            music = input()
+            playlist = YT.search(str(music))
+            if not playlist:
+                self.say("Sorry, there is not %s on youtube" % str(music))
                 self.delete()
             else:
-                self.MP = Music_Player(tracks,maximum=3)
+                self.MP = Music_Player(playlist,maximum=10)
                 self.MP.youtube_player()
 
-        if text == 'add music':
-            YT.flag = True
+        elif text == 'add music':
+            YT.is_existed_playlist = True
             self.MP.pause_vlc()
-            self.say("Please, write song\'s name")
+            self.say("Please, write music\'s name")
             self.delete()
-            song = input()
-            tracks2 = YT.search(str(song))
-            if not tracks2:
-                self.say("Sorry, there is not %s on youtube" % str(song))
+            music = input()
+            playlist_ = YT.search(str(music),len(self.MP.playlist))
+            if not playlist_:
+                self.say("Sorry, there is not %s on youtube" % str(music))
                 self.delete()
                 self.MP.play_vlc()
             else:
-                self.MP.tracks = {**self.MP.tracks, **tracks2}
+                self.MP.playlist = {**self.MP.playlist, **playlist_}
                 self.say("Music is added to your playlist")
                 self.delete()
                 self.MP.play_vlc()
 
-        if text == 're-start music':
+        elif text == 'resume music':
             self.say("Music will be started soon again")
             self.delete()
             self.MP.play_vlc()
 
-        if text == 'show playlist':
+        elif text == 'show playlist':
             self.MP.show_playlist()
 
-        if text == 'pause music':
+        elif text == 'pause music':
             self.MP.pause_vlc()
             self.say("Music is paused")
             self.delete()
 
-        if text == 'stop music':
+        elif text == 'stop music':
             self.MP.stop_vlc()
             self.say("Music is stopped")
             self.delete()
 
-        if text == 'next song':
+        elif text == 'next music':
             self.MP.stop_vlc()
             self.say("Music will be changed in a moment")
             self.delete()
             self.MP.next_song()
 
-        if text == 'question':
+        elif text == 'play':
+            self.say("We have one game on this AI speaker. Do you want you play with me? If you want to hear how to play, please write 'how to play'")
+            self.delete()
+            ans = input('Please, Write yes, no or how to play.\n')
+
+            while ans.lower() == 'yes':
+                self.say("The game is initialized.")
+                self.delete()
+                GG = q_learning()
+                GG.guess_number()
+                current_number = GG.computer.first_digit
+                chances = 20
+                number = -1
+
+                while chances > 0:
+                    self.say("Current number is %s. You have %s chance to guess the number" %(current_number, chances))
+                    print("Current number is %s. You have %s chance to guess the number" %(current_number, chances))
+                    self.delete()
+                    if 0 < number < GG.answer.first_digit:
+                        self.say("The answer number is greater than the number you've chosen.")
+                        self.delete()
+                    elif GG.answer.first_digit < number <= 50:
+                        self.say("The answer number is smaller than the number you've chosen.")
+                        self.delete()
+                    chances -= 1
+                    obs = GG.computer - GG.answer
+                    choice = np.argmax(GG.q_table[obs])
+                    GG.computer.action(choice)
+                    number = input("number (1~50) :  ")
+                    number = int(number)
+                    current_number = number
+                    if number == GG.answer.first_digit:
+                        self.say("You chose the number which is answer. You win.")
+                        self.delete()
+                        break
+                    if GG.computer.first_digit == GG.answer.first_digit:
+                        self.say("You are defeated. I chose answer number before you.")
+                        self.delete()
+                        break
+
+                ans = input('Please, Write yes, no or how to play.\n')
+
+            if ans.lower() == 'how to play':
+                self.say("You have to guess the number which is randomly picked by numpy library. You just need to guess before me.")
+                self.delete()
+                self.say("You have 20 chances to guess the number same as me. However, I can figure it out the number before you.")
+                self.delete()
+                self.say("There will be starting number and I will tell you whether this number is lower than the answer number or not")
+                self.delete()
+                self.say("The number is between 1 to 50.")
+                self.delete()
+
+        elif text == 'question':
             ques = "a"
             while ques.lower() != 'nothing':
                 if ques.lower() != "a":
@@ -89,10 +143,13 @@ class speaker:
                     self.say("What is your question?")
                     self.delete()
                 ques = input("question : ")
+                if "cammy" in ques.lower():
+                    self.say("Cammy is a beautiful girl who is sammy's girlfriend.")
+                    self.delete()
             self.say("Question end")
             self.delete()
 
-        if text == 'morning':
+        elif text == 'morning':
             self.say("Good morning ! How are you, sammy?")
             self.delete()
 
@@ -104,12 +161,8 @@ class speaker:
 if __name__ == '__main__':
     sp = speaker()
     YT = Youtube_Player(maximum=3)
+    start_q_table = {}
 
     while True:
         text = input("write command here\n")
-
-        if text.lower() == 'delete':
-            sp.delete()
-
-        else:
-            sp.main(text.lower())
+        sp.main(text.lower())
