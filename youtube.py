62296d363758eab8e1bb58fa69f61bfcb606f1e2
Sam.An <56215891+sammiee5311@users.noreply.github.com>
2020-10-27 23:21:41 +0900

    Update youtube.py

--- youtube.py
+++ youtube.py
@@ -5,77 +5,59 @@ import collections
 
 
 class Youtube_Player:
-    def __init__(self, maximum=1, flag=False):
-        self.DEVELOPER_KEY = ''
+    def __init__(self, maximum=1):
+        self.DEVELOPER_KEY = 'AIzaSyAuOaIjo2G-hugN6uM_XnOcJyziy74TJQw'
         self.YOUTUBE_API_SERVICE_NAME = 'youtube'
         self.YOUTUBE_API_VERSION = 'v3'
         self.maximum = maximum
-        self.flag = flag
-        self.cnt = 0
+        self.is_existed_playlist = False
+        self.track_number = 0
 
-    def check(self, readData):
-        for text in readData:
+    def is_contained_text(self, title):
+        for text in title:
             text = re.sub('[-=+,#/\?:^$.@*\"※~&%ㆍ!』\\‘|\(\)\[\]\<\>`\'…》]', '', text)
             if text.lower() in ['lyrics','audio']:
                 return True
-
         return False
 
-    def youtube_stream_link(self, videos):
-        if self.maximum == 1:
-            url = 'https://www.youtube.com/watch?v='+str(videos[1][0][-11:])
+    def youtube_stream_link(self, playlist, len_of_playlist):
+        self.track_number = len_of_playlist
+        while self.track_number < len_of_playlist + len(playlist):
+            self.track_number += 1
+            url = 'https://www.youtube.com/watch?v=' + str(playlist[self.track_number][0][-11:])
             video = pafy.new(url)
             best_audio = video.getbestaudio()
             audio_streaming_link = best_audio.url
-            videos[1].append(audio_streaming_link)
-        else:
-            if self.flag:
-                self.cnt = len(videos)
-                x = self.cnt
-                while self.cnt < len(videos) + x:
-                    self.cnt += 1
-                    url = 'https://www.youtube.com/watch?v=' + str(videos[self.cnt][0][-11:])
-                    video = pafy.new(url)
-                    best_audio = video.getbestaudio()
-                    audio_streaming_link = best_audio.url
-                    videos[self.cnt].append(audio_streaming_link)
-            else:
-                self.cnt = 0
-                while self.cnt < len(videos):
-                    self.cnt += 1
-                    url = 'https://www.youtube.com/watch?v=' + str(videos[self.cnt][0][-11:])
-                    video = pafy.new(url)
-                    best_audio = video.getbestaudio()
-                    audio_streaming_link = best_audio.url
-                    videos[self.cnt].append(audio_streaming_link)
+            playlist[self.track_number].append(audio_streaming_link)
 
-        return videos
+        return playlist
 
-    def search(self, query):
+    def search(self, query, len_of_playlist=0):
         youtube_client = build(self.YOUTUBE_API_SERVICE_NAME, self.YOUTUBE_API_VERSION,
                         developerKey=self.DEVELOPER_KEY)
 
         search_response = youtube_client.search().list(
-            q = query,
-            part = 'id, snippet'
+            q=query,
+            part='id, snippet'
         ).execute()
 
-        videos = collections.defaultdict(list)
+        playlist = collections.defaultdict(list)
 
-        if self.flag:
+        if self.is_existed_playlist:
             pass
         else:
-            self.cnt = 0
+            self.track_number = 0
 
         for search_result in search_response.get('items', []):
             if search_result['id']['kind'] == 'youtube#video':
-                if self.check(search_result['snippet']['title'].split()):
-                    self.cnt += 1
-                    videos[self.cnt].append('%s %s' % (search_result['snippet']['title'],
-                                               search_result['id']['videoId']))
+                if self.is_contained_text(search_result['snippet']['title'].split()):
+                    self.track_number += 1
+                    playlist[self.track_number].append('%s %s' % (search_result['snippet']['title'], search_result['id']['videoId']))
+                if self.maximum == self.track_number:
+                    break
 
-        if len(videos) != 0:
-            audio_streaming_link = self.youtube_stream_link(videos)
+        if len(playlist) != 0:
+            audio_streaming_link = self.youtube_stream_link(playlist,len_of_playlist)
             return audio_streaming_link
 
 
