0cf763ac44ce441eee1348c00ce397b8dad8411d
Sam.An <56215891+sammiee5311@users.noreply.github.com>
2020-12-16 19:58:45 +0900

    Update main_window.py

--- gui/main_window.py
+++ gui/main_window.py
@@ -2,9 +2,8 @@ import os
 import os.path
 import numpy as np
 from PyQt5.QtWidgets import QDialog, QMessageBox
-from PyQt5.QtGui import QImage, QPixmap
 from PyQt5.uic import loadUi
-from PyQt5.QtCore import pyqtSlot, QTimer, Qt
+from PyQt5.QtCore import pyqtSlot, Qt, QThread, pyqtSignal, QObject, QTimer
 from PyQt5 import QtWidgets
 from gtts import gTTS
 from playsound import playsound
@@ -16,6 +15,24 @@ from guessing_game import q_learning
 ######################################
 
 
+class CommandSignals(QObject):
+    command = pyqtSignal(str)
+
+
+class Worker(QObject):
+    def __init__(self, parent=None):
+        super(self.__class__, self).__init__(parent)
+        self.signals = CommandSignals()
+        print("One Thread has been started")
+
+    @pyqtSlot(str)
+    def say(self,sentence):
+        print(sentence)
+        tts = gTTS(text=sentence, lang='en')
+        tts.save("./tmp/a.mp3")
+        playsound("./tmp/a.mp3")
+        
+
 class Main_Window(QtWidgets.QMainWindow):
     def __init__(self):
         super(Main_Window, self).__init__()
@@ -26,18 +43,24 @@ class Main_Window(QtWidgets.QMainWindow):
         self.webcam_button.clicked.connect(self.start_webcam)
         self.input = self.findChild(QtWidgets.QLineEdit, 'chat_input_label')
         self.filename = "./tmp/a.mp3"
+        self.signals = CommandSignals()
+        self.signals2 = CommandSignals()
+        self.worker = Worker()
+        self.worker_thread = QThread()
+        self.start_thread()
         self.current_statue = None
         self.MP = None
         self.new_window = None
+        
+    def end_thread(self):
+        if self.worker_thread.isRunning():
+            self.worker_thread.terminate()
 
-    def say(self,sentence):
-        tts = gTTS(text=sentence, lang='en')
-        tts.save(self.filename)
-        playsound(self.filename)
-
-    def delete(self):
-        if os.path.isfile(self.filename):
-            os.remove(self.filename)
+    def start_thread(self):
+        self.signals.command.connect(self.worker.say)
+        self.signals2.command.connect(self.worker.music)
+        self.worker.moveToThread(self.worker_thread)
+        self.worker_thread.start()
 
     def keyPressEvent(self, e):
         if e.key() == Qt.Key_Escape:
@@ -60,18 +83,19 @@ class Main_Window(QtWidgets.QMainWindow):
     def start_game(self):
         if self.MP:
             self.MP.pause_vlc()
+        if not self.worker_thread.isRunning():
+            self.worker_thread.start()
         self.current_statue = 'game'
         self.statue.setText(self.current_statue)
-        # self.say(
-        #     "We have one game on this AI speaker. Do you want you play with me? If you want to hear how to play, please write 'how to play'")
+        self.signals.command.emit("We have one game on this AI speaker. Do you want you play with me? If you want to hear how to play, please write 'how to play'")
         self.chat_output_label.setText('Please, Write yes, no or how to play.')
         self.chat_output_label.adjustSize()
-        self.delete()
+        # self.delete()
 
-        ans = self.input.text()
+        ans = 'how to play'
 
         while ans.lower() == 'yes':
-            self.say("The game is initialized.")
+            self.signals.command.emit("The game is initialized.")
             self.delete()
             GG = q_learning()
             GG.guess_number()
@@ -80,16 +104,16 @@ class Main_Window(QtWidgets.QMainWindow):
             number = -1
 
             while chances > 0:
-                self.say("Current number is %s. You have %s chance to guess the number" % (current_number, chances))
+                self.signals.command.emit("Current number is %s. You have %s chance to guess the number" % (current_number, chances))
                 self.delete()
                 self.chat_output_label.setText(
                     "Current number is %s. You have %s chance to guess the number" % (current_number, chances))
                 self.chat_output_label.adjustSize()
                 if 0 < number < GG.answer.first_digit:
-                    self.say("The answer number is greater than the number you've chosen.")
+                    self.signals.command.emit("The answer number is greater than the number you've chosen.")
                     self.delete()
                 elif GG.answer.first_digit < number <= 50:
-                    self.say("The answer number is smaller than the number you've chosen.")
+                    self.signals.command.emit("The answer number is smaller than the number you've chosen.")
                     self.delete()
                 chances -= 1
                 obs = GG.computer - GG.answer
@@ -99,27 +123,27 @@ class Main_Window(QtWidgets.QMainWindow):
                 number = int(number)
                 current_number = number
                 if number == GG.answer.first_digit:
-                    self.say("You chose the number which is answer. You win.")
+                    self.signals.command.emit("You chose the number which is answer. You win.")
                     self.delete()
                     break
                 if GG.computer.first_digit == GG.answer.first_digit:
-                    self.say("You are defeated. I chose answer number before you.")
+                    self.signals.command.emit("You are defeated. I chose answer number before you.")
                     self.delete()
                     break
 
             ans = input('Please, Write yes, no or how to play.\n')
 
         if ans.lower() == 'how to play':
-            self.say(
+            self.signals.command.emit(
                 "You have to guess the number which is randomly picked by numpy library. You just need to guess before me.")
             self.delete()
-            self.say(
+            self.signals.command.emit(
                 "You have 20 chances to guess the number same as me. However, I can figure it out the number before you.")
             self.delete()
-            self.say(
+            self.signals.command.emit(
                 "There will be starting number and I will tell you whether this number is lower than the answer number or not")
             self.delete()
-            self.say("The number is between 1 to 50.")
+            self.signals.command.emit("The number is between 1 to 50.")
             self.delete()
 
         self.current_statue = None
@@ -128,6 +152,7 @@ class Main_Window(QtWidgets.QMainWindow):
     def get_weather_information(self):
         weather = Weather_Api()
         current_weather = weather.get_current_weather()
+        self.signals.command.emit("Current weather is " + current_weather)
         self.chat_output_label.setText("Current weather is " + current_weather)
         self.chat_output_label.adjustSize()
 
@@ -147,6 +172,8 @@ class Main_Window(QtWidgets.QMainWindow):
             self.start_youtube_music()
         elif text.lower() == 'webcam':
             self.start_webcam()
+        elif text.lower() == 'end_thread':
+            self.end_thread()
         else:
             self.chat_output_label.setText("This command is not in the list")
         self.chat_output_label.adjustSize()
